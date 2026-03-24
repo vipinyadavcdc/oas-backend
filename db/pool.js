948072e2c5f7bc86ab5066378708073be00282b9
vipinyadavcdc <vipinyadavcdc@gmail.com>
2026-03-24 23:01:37 +0530

    Add files via upload

--- db/pool.js
+++ db/pool.js
@@ -2,9 +2,9 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  max: 20,
+  max: 50,
   idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 2000,
+  connectionTimeoutMillis: 5000,
   ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
 });
 
